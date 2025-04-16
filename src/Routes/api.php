92ebdd362b5327d2b96db82d9db8514bf29ace96
mtorres-lechuganegra <marquillo01@gmail.com>
2025-04-15 19:38:08 -0500

    Add name routes

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use LechugaNegra\AuthManager\Http\Controllers\AuthController;
 
-Route::prefix('api/auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('api/auth')->name('api.auth.')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('login'); // Inicia sesión
 });
 
 Route::middleware(['auth:api'])->group(function () {
-    Route::prefix('api/auth')->group(function () {
-        Route::get('/me', [AuthController::class, 'me']);
-        Route::post('/validate', [AuthController::class, 'validate']);
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::post('/logout', [AuthController::class, 'logout']);
+    Route::prefix('api/auth')->name('api.auh.')->group(function () {
+        Route::get('/me', [AuthController::class, 'me'])->name('me'); // Obtiene datos de usuario autenticado
+        Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh'); // Refresca token de autenticación
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); // Cierra sesión
+        Route::post('/validate', [AuthController::class, 'validate'])->name('validate'); // Valida token de autenticación
     });
 });
